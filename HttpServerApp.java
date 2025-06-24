3eda3358d611b600c410a33d27f0f22cd2d76569
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-06-24 15:55:08 +0500

    Update HttpServerApp.java

--- HttpServerApp.java
+++ HttpServerApp.java
@@ -17,7 +17,7 @@ public class HttpServerApp {
 
     static class MyHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello from Java updated task!";
+            String response = "Hello from Java updated task hurrah!!!!!!!!!!!!!!!!!!!!!!!!!1!";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
