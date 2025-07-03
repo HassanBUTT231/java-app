3bde08cd399ae3a700f89ccb438106e98b904182
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-07-03 12:01:15 +0500

    Create start_container.sh

---
+++ scripts/start_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Set your values
+REGION="us-east-1"
+ACCOUNT_ID="099199746132"
+REPO_NAME="lumino-java-app"
+IMAGE_TAG="latest"
+
+echo "Logging into Amazon ECR..."
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com"
+
+echo "Pulling the Docker image..."
+docker pull "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG"
+
+echo "Running the Docker container..."
+docker run -d -p 5000:5000 "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG"
